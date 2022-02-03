a8bfb2c3589a0f25b0b0aaac2b6da2262ac684b9
Valeri Hristov <valericfbg@gmail.com>
2022-02-03 15:39:53 +0200

    factoriel

--- softuni-algo/recursion/Recursion.cpp
+++ softuni-algo/recursion/Recursion.cpp
@@ -1,5 +1,7 @@
 #include "Recursion.h"
 
+#include <cassert>
+
 int64_t Recursion::sum(int32_t const* arr, size_t n) {
 	return sum(arr, 0, n);
 }
@@ -8,4 +10,15 @@ int64_t Recursion::sum(int32_t const* arr, size_t idx, size_t n) {
 	if (idx >= n) return 0;
 
 	return arr[idx] + sum(arr, idx + 1, n);
+}
+
+uint64_t Recursion::factoriel(const uint8_t n) {
+	assert(n <= 20);
+	return factoriel(1, n);
+}
+
+uint64_t Recursion::factoriel(const uint8_t current, const uint8_t n) {
+	if (current > n) return 1;
+
+	return current * factoriel(current + 1, n);
 }
--- softuni-algo/recursion/Recursion.h
+++ softuni-algo/recursion/Recursion.h
@@ -9,8 +9,10 @@ public:
 	Recursion() = default;
 
 	int64_t sum(int32_t const* arr, size_t n);
+	uint64_t factoriel(const uint8_t n);
 private:
 	int64_t sum(int32_t const* arr, size_t idx, size_t n);
+	uint64_t factoriel(const uint8_t current, const uint8_t n);
 };
 
 #endif /* SOFTUNI_ALGO_RECURSION_RECURSION_H_ */
