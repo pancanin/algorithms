e0003b3831a756966fa0dc74b4a6263add480c2a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-02-07 17:13:50 -0500

    some progress on merge sort

--- softuni-algo/sorting/Sorting.cpp
+++ softuni-algo/sorting/Sorting.cpp
@@ -4,21 +4,23 @@
 void Sorting::mergeSort(int* arr, int32_t n) {
 	int32_t* dest = new int[n];
 	mergeSort(arr, 0, n - 1, dest);
-
-	arr = dest;
 }
 
 void Sorting::merge(int32_t* a, int32_t* b, int32_t aSize, int32_t bSize, int32_t* destination, int32_t destStart) {
+	int32_t* temp = new int32_t[aSize + bSize];
+	std::cout << "Merge method called with parameters " << aSize << " and " << bSize << std::endl;
 	int32_t aIdx = 0;
 	int32_t bIdx = 0;
 	int32_t destIdx = destStart;
 
 	while (aIdx < aSize && bIdx < bSize) {
 		if (a[aIdx] <= b[bIdx]) {
-			destination[destIdx] = a[aIdx];
+			std::cout << "Selecting " << a[aIdx] << std::endl;
+			temp[destIdx] = a[aIdx];
 			aIdx++;
 		} else {
-			destination[destIdx] = b[bIdx];
+			std::cout << "Selecting " << b[bIdx] << std::endl;
+			temp[destIdx] = b[bIdx];
 			bIdx++;
 		}
 
@@ -26,24 +28,30 @@ void Sorting::merge(int32_t* a, int32_t* b, int32_t aSize, int32_t bSize, int32_
 	}
 
 	while (aIdx < aSize) {
-		destination[destIdx++] = a[aIdx++];
+		std::cout << "Selecting " << a[aIdx] << std::endl;
+		temp[destIdx++] = a[aIdx++];
 	}
 
 	while (bIdx < bSize) {
-		destination[destIdx++] = b[bIdx++];
+		std::cout << "Selecting " << b[bIdx] << std::endl;
+		temp[destIdx++] = b[bIdx++];
+	}
+
+	for (int32_t i = destStart; i < destStart + aSize + bSize; i++) {
+		destination[i] = temp[i - destStart];
 	}
 }
 
 void Sorting::mergeSort(int32_t* arr, int32_t low, int32_t high, int32_t* destination) {
 	if (high <= low) {
+		std::cout << "Here we have an array of size 1: " << arr[low] << std::endl;
 		return;
 	}
 
 	int32_t mid = low + ((high - low) / 2);
-	std::cout << "Pre Low: " << low << "; high " << high << "; Mid is " << mid << std::endl;
 
 	mergeSort(arr, low, mid, destination);
 	mergeSort(arr, mid + 1, high, destination);
-
-	std::cout << "Post Low: " << low << "; Mid is " << mid << "; high " << high << std::endl;
+	merge(&arr[low], &arr[mid + 1], mid - low + 1, high - mid, arr, low);
+	std::cout << "Backtracking: low is: " << low << " and mid is: " << mid << " and high is " << high << std::endl;
 }
